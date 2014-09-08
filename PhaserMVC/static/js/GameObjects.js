b1ca908c116ef5f1099f9f71e3ff348d62d891b8
Josh O'Bryan <vespergo@gmail.com>
2014-09-07 20:01:14 -0700

    fix for game piece not pushing back to original position

--- PhaserMVC/static/js/GameObjects.js
+++ PhaserMVC/static/js/GameObjects.js
@@ -1,29 +1,29 @@
 ﻿/// <reference path="main.js" />
 function Card(point, group) {
     this.position = point;
-    this.cardImg = game.add.sprite(this.position.x, this.position.y, 'cardFrameSheet', 4, group);
+    this.cardImg = game.add.sprite(this.position.x, this.position.y, 'cardFrameSheet', 4, group);     
     this.origPos = CopyObject(point);
     this.cardImg.inputEnabled = true;
     this.cardImg.input.enableDrag(true);
-    this.cardImg.events.onDragStop.add(this.dragStop);
+    this.cardImg.events.onDragStop.add(this.dragStop, this);
 }
 
-Card.prototype.dragStop = function(card){
+Card.prototype.dragStop = function(cardImg){
     //lets check to see if it landed on the board, otherwise return to original location
     var onBoard = false;
-    var cardCenter = {x: card.position.x+card.width/2, y: card.position.y+card.height/2};
+    var cardCenter = {x: cardImg.position.x+cardImg.width/2, y: cardImg.position.y+cardImg.height/2};
         
     for(var i=0; i<mainState.emptyGameBoardHexes.length; i++){
         var boardHex = mainState.emptyGameBoardHexes.getAt(i);
         if(boardHex.position.x < cardCenter.x && cardCenter.x < boardHex.position.x + boardHex.width && boardHex.position.y < cardCenter.y && cardCenter.y < boardHex.position.y+boardHex.width){
             //found hex on board
             onBoard = true;
-            card.position = CopyObject(boardHex.position);
+            cardImg.position = CopyObject(boardHex.position);
             break;
         }
     }
     //return to origPos
     if(!onBoard){
-        card.position = CopyObject(card.origPos);
+        cardImg.position = CopyObject(this.origPos);
     }
 }
--- PhaserMVC/static/js/main.js
+++ PhaserMVC/static/js/main.js
@@ -33,7 +33,7 @@ var mainState = {
     },
 
     update: function() {
-
+       
     },
 
     buildGameBoard: function () {
