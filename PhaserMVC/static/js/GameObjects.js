f38bca4b929f58926c188e86dc29540fa1bfe724
Jason Marlow <jasonmarlow@gmail.com>
2014-09-12 00:24:28 -0600

    Scaling

--- PhaserMVC/static/js/GameObjects.js
+++ PhaserMVC/static/js/GameObjects.js
@@ -2,19 +2,23 @@
 /// <reference path="main.js" />
 /// <reference path="phaser.js" />
 /// <reference path="main.js" />
-Card = function (point, group, elementNum, frameNum) {
+Card = function (point, group, elementNum, frameNum, cardScale) {
     this.position = point;
     var cardImg = game.add.sprite(this.position.x, this.position.y, 'elementalBGs', elementNum, group);
     cardImg.anchor.setTo(0.5, 0.5);
+    cardImg.scale.setTo(cardScale);
 
-    var buffer = 3;
-    var frameImage = game.add.sprite(-cardImg.width / 2, -cardImg.height / 2, 'cardFrameSheet', frameNum)
-    cardImg.anchor.setTo(0.5, 0.5);
-    var numberLeft = game.add.sprite(-cardImg.width / 2+buffer, -cardImg.height/4+buffer, 'numberSheet', 1); 
-    var numberBottom = game.add.sprite(0, cardImg.height/2.3+buffer, 'numberSheet', 7);   
-    numberBottom.anchor.setTo(0.5, 1);
-    var numberRight = game.add.sprite(cardImg.width / 3+buffer, -cardImg.height/4+buffer, 'numberSheet', 3);
-    var character = game.add.sprite(-cardImg.width / 2, -cardImg.height / 2 - (Math.round(32 * scale)), 'wolf')
+    var frameImage = game.add.sprite(0, 0, 'cardFrameSheet', frameNum)
+    var numberLeft = game.add.sprite(-62, -20, 'numberSheet', 1);
+    var numberBottom = game.add.sprite(0, 53, 'numberSheet', 7);
+    var numberRight = game.add.sprite(62, -20, 'numberSheet', 3);
+    var character = game.add.sprite(0, -32, 'wolf');
+
+    numberLeft.anchor.setTo(0.5, 0.5);
+    numberBottom.anchor.setTo(0.5, 0.5);
+    numberRight.anchor.setTo(0.5, 0.5);
+    frameImage.anchor.setTo(0.5, 0.5);
+    character.anchor.setTo(0.5, 0.5);
 
     //we'll bing all the sprites to the cardImg one and then they'll follow the cardImg as it's dragged
     cardImg.addChild(frameImage);
@@ -23,23 +27,23 @@ Card = function (point, group, elementNum, frameNum) {
     cardImg.addChild(numberRight);
     cardImg.addChild(character);
     cardImg.hitArea = new Phaser.Circle(0, 0, 125);
-    
+
     this.origPos = CopyObject(point);
     cardImg.inputEnabled = true;
     cardImg.input.enableDrag(true);
     cardImg.events.onDragStop.add(this.dragStop, this);
-    
+
 }
 
-Card.prototype.dragStop = function(cardImg){
+Card.prototype.dragStop = function (cardImg) {
     //lets check to see if it landed on the board, otherwise return to original location
     var onBoard = false;
-    var cardCenter = {x: cardImg.position.x+cardImg.width/2, y: cardImg.position.y+cardImg.height/2};
-        
-    for(var i=0; i<mainState.emptyGameBoardHexes.length; i++){
+    var cardCenter = { x: cardImg.position.x + cardImg.width / 2, y: cardImg.position.y + cardImg.height / 2 };
+
+    for (var i = 0; i < mainState.emptyGameBoardHexes.length; i++) {
         var boardHex = mainState.emptyGameBoardHexes.getAt(i);
-        if(boardHex.position.x < cardCenter.x && cardCenter.x < boardHex.position.x + boardHex.width &&
-         boardHex.position.y < cardCenter.y && cardCenter.y < boardHex.position.y+boardHex.width){
+        if (boardHex.position.x < cardCenter.x && cardCenter.x < boardHex.position.x + boardHex.width &&
+         boardHex.position.y < cardCenter.y && cardCenter.y < boardHex.position.y + boardHex.width) {
             //found hex on board
             onBoard = true;
             cardImg.position = CopyObject(boardHex.position);
@@ -47,7 +51,7 @@ Card.prototype.dragStop = function(cardImg){
         }
     }
     //return to origPos
-    if(!onBoard){
+    if (!onBoard) {
         cardImg.position = CopyObject(this.origPos);
     }
 }
--- PhaserMVC/static/js/main.js
+++ PhaserMVC/static/js/main.js
@@ -1,12 +1,30 @@
 /// <reference path="phaser.js" />
 /// <reference path="GameObject.js" />
-var game = new Phaser.Game(768, 954, Phaser.AUTO, 'mainDiv');
-var scale = 1;
+
+//DISPLAY
+var canvWidth;
+var canvHeight;
+var screenRatio = (window.innerWidth / window.innerHeight).toFixed(4);
+
+//desktop
+if (screenRatio >= 0.8767 && window.innerWidth >= 768) {
+    canvWidth = 768;
+    canvHeight = window.innerHeight;
+}
+    //mobile
+else {
+    canvWidth = window.innerWidth;
+    canvHeight = window.innerHeight;
+}
+
+var game = new Phaser.Game(canvWidth, canvHeight, Phaser.AUTO, 'mainDiv');
+var globalScale = (canvWidth / 768).toFixed(4);
+// var globalScale = 1;
 
 var mainState = {
     //internal vars
     emptyGameBoardHexes: {},
-    
+
     preload: function () {
         //Load Art Assets
         game.load.image('gameBoard', 'img/GameBoard.png');
@@ -15,7 +33,7 @@ var mainState = {
         game.load.spritesheet('numberSheet', 'img/numbers.png', 18, 22, 33, 1, 2);
         game.load.spritesheet('cardFrameSheet', 'img/FrameSheet.png', 150, 140, 6);
         game.load.spritesheet('elementalBGs', 'img/ElementalBGs.png', 150, 140, 6);
-        
+
     },
 
     create: function () {
@@ -24,75 +42,75 @@ var mainState = {
         //Add boards
         this.buildGameBoard();
         this.buildHandBoard();
-        
-        // Display the names
-        this.playerName = game.add.text(10, 5, 'Player Name',
-        { font: '30px Arial', fill: '#ffffff' });
 
-        this.opponentName = game.add.text(game.world.width - 190, 5, 'Other Name',
-        { font: Math.floor(30 * scale) + 'px Arial', fill: '#ffffff' });
+        // Display the names
+        this.playerName = game.add.text(10, 5, screenRatio,
+        { font: Math.floor(30 * globalScale) + 'px Arial', fill: '#ffffff' });
+
+        this.opponentName = game.add.text(game.world.width - 190, 5, 'scale: ' + globalScale,
+        { font: Math.floor(30 * globalScale) + 'px Arial', fill: '#ffffff' });
     },
 
-    update: function() {
+    update: function () {
         for (var i = 0; i < this.playerHand.length; i++) {
             this.playerHand.getAt(i).update();
         }
     },
 
     buildGameBoard: function () {
-        this.gameBoard = game.add.sprite(game.world.centerX, Math.round(game.world.centerY - 100 * scale), 'gameBoard');
+        this.gameBoard = game.add.sprite(game.world.centerX, 0 + Math.round(350 * globalScale), 'gameBoard');
         this.gameBoard.anchor.setTo(0.5, 0.5);
+        this.gameBoard.scale.setTo(globalScale);
 
         // Create a group of empty hexes
         this.emptyGameBoardHexes = game.add.group();
         var gameBoardPositions = [
         //1st Row
             //Hex0
-            { x: Math.round((this.gameBoard.x - 150) * scale), y: Math.round((this.gameBoard.y - 204) * scale) },
+            { x: Math.round(this.gameBoard.x - 150 * globalScale), y: Math.round(this.gameBoard.y - 204 * globalScale) },
             //Hex1
-            { x: Math.round((this.gameBoard.x + 0) * scale), y: Math.round((this.gameBoard.y - 204) * scale) },
+            { x: Math.round(this.gameBoard.x + 0), y: Math.round(this.gameBoard.y - 204 * globalScale) },
             //Hex2
-            { x: Math.round((this.gameBoard.x + 150) * scale), y: Math.round((this.gameBoard.y - 204) * scale) },
-
+            { x: Math.round(this.gameBoard.x + 150 * globalScale), y: Math.round(this.gameBoard.y - 204 * globalScale) },
         //2nd Row
             //Hex3
-            { x: Math.round((this.gameBoard.x - 225) * scale), y: Math.round((this.gameBoard.y - 102) * scale) },
+            { x: Math.round(this.gameBoard.x - 225 * globalScale), y: Math.round(this.gameBoard.y - 102 * globalScale) },
             //Hex4
-            { x: Math.round((this.gameBoard.x - 75) * scale), y: Math.round((this.gameBoard.y - 102) * scale) },
+            { x: Math.round(this.gameBoard.x - 75 * globalScale), y: Math.round(this.gameBoard.y - 102 * globalScale) },
             //Hex5
-            { x: Math.round((this.gameBoard.x + 75) * scale), y: Math.round((this.gameBoard.y - 102) * scale) },
+            { x: Math.round(this.gameBoard.x + 75 * globalScale), y: Math.round(this.gameBoard.y - 102 * globalScale) },
             //Hex6
-            { x: Math.round((this.gameBoard.x + 225) * scale), y: Math.round((this.gameBoard.y - 102) * scale) },
+            { x: Math.round(this.gameBoard.x + 225 * globalScale), y: Math.round(this.gameBoard.y - 102 * globalScale) },
 
         //3rd Row
             //Hex7
-            { x: Math.round((this.gameBoard.x - 300) * scale), y: Math.round((this.gameBoard.y + 0) * scale) },
+            { x: Math.round(this.gameBoard.x - 300 * globalScale), y: Math.round(this.gameBoard.y + 0) },
             //Hex8
-            { x: Math.round((this.gameBoard.x - 150) * scale), y: Math.round((this.gameBoard.y + 0) * scale) },
+            { x: Math.round(this.gameBoard.x - 150 * globalScale), y: Math.round(this.gameBoard.y + 0) },
             //Hex9
-            { x: Math.round((this.gameBoard.x + 0) * scale), y: Math.round((this.gameBoard.y + 0) * scale) },
+            { x: Math.round(this.gameBoard.x + 0), y: Math.round(this.gameBoard.y + 0) },
             //Hex10
-            { x: Math.round((this.gameBoard.x + 150) * scale), y: Math.round((this.gameBoard.y + 0) * scale) },
+            { x: Math.round(this.gameBoard.x + 150 * globalScale), y: Math.round(this.gameBoard.y + 0) },
             //Hex11
-            { x: Math.round((this.gameBoard.x + 300) * scale), y: Math.round((this.gameBoard.y + 0) * scale) },
+            { x: Math.round(this.gameBoard.x + 300 * globalScale), y: Math.round(this.gameBoard.y + 0) },
 
         //4th Row
             //Hex12
-            { x: Math.round((this.gameBoard.x - 225) * scale), y: Math.round((this.gameBoard.y + 102) * scale) },
+            { x: Math.round(this.gameBoard.x - 225 * globalScale), y: Math.round(this.gameBoard.y + 102 * globalScale) },
             //Hex13
-            { x: Math.round((this.gameBoard.x - 75) * scale), y: Math.round((this.gameBoard.y + 102) * scale) },
+            { x: Math.round(this.gameBoard.x - 75 * globalScale), y: Math.round(this.gameBoard.y + 102 * globalScale) },
             //Hex14
-            { x: Math.round((this.gameBoard.x + 75) * scale), y: Math.round((this.gameBoard.y + 102) * scale) },
+            { x: Math.round(this.gameBoard.x + 75 * globalScale), y: Math.round(this.gameBoard.y + 102 * globalScale) },
             //Hex15
-            { x: Math.round((this.gameBoard.x + 225) * scale), y: Math.round((this.gameBoard.y + 102) * scale) },
+            { x: Math.round(this.gameBoard.x + 225 * globalScale), y: Math.round(this.gameBoard.y + 102 * globalScale) },
 
         //5th Row
             //Hex16
-            { x: Math.round((this.gameBoard.x - 150) * scale), y: Math.round((this.gameBoard.y + 204) * scale) },
+            { x: Math.round(this.gameBoard.x - 150 * globalScale), y: Math.round(this.gameBoard.y + 204 * globalScale) },
             //Hex17
-            { x: Math.round((this.gameBoard.x + 0) * scale), y: Math.round((this.gameBoard.y + 204) * scale) },
+            { x: Math.round(this.gameBoard.x + 0), y: Math.round(this.gameBoard.y + 204 * globalScale) },
             //Hex18
-            { x: Math.round((this.gameBoard.x + 150) * scale), y: Math.round((this.gameBoard.y + 204) * scale) },
+            { x: Math.round(this.gameBoard.x + 150 * globalScale), y: Math.round(this.gameBoard.y + 204 * globalScale) },
         ];
 
         this.emptyGameBoardHexes.createMultiple(19, 'cardFrameSheet', 0, true);
@@ -100,42 +118,44 @@ var mainState = {
             this.emptyGameBoardHexes.getAt(i).x = gameBoardPositions[i].x;
             this.emptyGameBoardHexes.getAt(i).y = gameBoardPositions[i].y;
             this.emptyGameBoardHexes.getAt(i).anchor.setTo(0.5, 0.5);
+            this.emptyGameBoardHexes.getAt(i).scale.setTo(globalScale);
         }
     },
     buildHandBoard: function () {
-        this.handBoard = game.add.sprite(game.world.centerX, game.world.height - 140 * scale, 'handBoard');
+        this.handBoard = game.add.sprite(game.world.centerX, game.world.height - 140 * globalScale, 'handBoard');
         this.handBoard.anchor.setTo(0.5, 0.5);
+        this.handBoard.scale.setTo(globalScale);
 
         // Create a group of empty hexes
         this.playerHand = game.add.group();
         var handBoardPositions = [
         //1st Row
             //Hex0
-            { x: Math.round((this.handBoard.x - 225) * scale), y: Math.round((this.handBoard.y - 51) * scale) },
+            { x: Math.round(this.handBoard.x - 225 * globalScale), y: Math.round(this.handBoard.y - 51 * globalScale) },
             //Hex1
-            { x: Math.round((this.handBoard.x - 75) * scale), y: Math.round((this.handBoard.y - 51) * scale) },
+            { x: Math.round(this.handBoard.x - 75 * globalScale), y: Math.round(this.handBoard.y - 51 * globalScale) },
             //Hex2
-            { x: Math.round((this.handBoard.x + 75) * scale), y: Math.round((this.handBoard.y - 51) * scale) },
+            { x: Math.round(this.handBoard.x + 75 * globalScale), y: Math.round(this.handBoard.y - 51 * globalScale) },
             //Hex3
-            { x: Math.round((this.handBoard.x + 225) * scale), y: Math.round((this.handBoard.y - 51) * scale) },
+            { x: Math.round(this.handBoard.x + 225 * globalScale), y: Math.round(this.handBoard.y - 51 * globalScale) },
 
         //2nd Row
             //Hex4
-            { x: Math.round((this.handBoard.x - 300) * scale), y: Math.round((this.handBoard.y + 51) * scale) },
+            { x: Math.round(this.handBoard.x - 300 * globalScale), y: Math.round(this.handBoard.y + 51 * globalScale) },
             //Hex5
-            { x: Math.round((this.handBoard.x - 150) * scale), y: Math.round((this.handBoard.y + 51) * scale) },
+            { x: Math.round(this.handBoard.x - 150 * globalScale), y: Math.round(this.handBoard.y + 51 * globalScale) },
             //Hex6
-            { x: Math.round((this.handBoard.x + 0) * scale), y: Math.round((this.handBoard.y + 51) * scale) },
+            { x: Math.round(this.handBoard.x + 0), y: Math.round(this.handBoard.y + 51 * globalScale) },
             //Hex7
-            { x: Math.round((this.handBoard.x + 150) * scale), y: Math.round((this.handBoard.y + 51) * scale) },
+            { x: Math.round(this.handBoard.x + 150 * globalScale), y: Math.round(this.handBoard.y + 51 * globalScale) },
             //Hex8
-            { x: Math.round((this.handBoard.x + 300) * scale), y: Math.round((this.handBoard.y + 51) * scale) },
+            { x: Math.round(this.handBoard.x + 300 * globalScale), y: Math.round(this.handBoard.y + 51 * globalScale) },
         ];
 
         //create 9 cards
         for (var i = 0; i < 9; i++) {
             var point = { x: handBoardPositions[i].x, y: handBoardPositions[i].y };
-            var card = new Card(point, this.playerHand, 0, 5);
+            var card = new Card(point, this.playerHand, 0, 5, globalScale);
         }
     },
 };
